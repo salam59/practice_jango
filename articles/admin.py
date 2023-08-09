2188b9ce50f25ae0bf9a44d48e7680c283f06322
salam59 <r170059@rguktrkv.ac.in>
2023-08-09 18:39:23 +0530

    added articles model to the admin page where we can manage them, also added search option to search the articles in admin page

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+class articleAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    search_fields = ['id','title']
+
+admin.site.register(Article,articleAdmin)
